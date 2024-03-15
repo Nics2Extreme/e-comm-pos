f3591ae0d0d632af278312559e7650fe7297fa48
Harvey Arboleda <harveyarboleda@Harveys-MacBook-Air.local>
2024-03-15 23:36:47 +0800

    first commit

--- resources/views/livewire/order.blade.php
+++ resources/views/livewire/order.blade.php
@@ -29,7 +29,7 @@
                 <div class="bg-white shadow-lg mb-2 rounded-md p-4">
                     Total: P{{$total}}
                 </div>
-                <button id="checkout" type="button" class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Checkout</button>
+                <button id="checkout" type="button" class="{{$total == 0 ? 'disabled' : ''}}focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800" {{$total == 0 ? 'disabled' : ''}}>Checkout</button>
             </div>
             <div class="billing" style="display: none;">
                 <h1 class="text-2xl font-bold mb-2">Billing</h1>
